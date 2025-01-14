b945b2eb0aeef7ff3dfb6d4da3be6aac04d7e6ca
devMaikel <maikelsilvestre@velty.com.br>
2025-01-14 13:43:26 -0300

    projeto configurado com watch/hot-reload

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -4,29 +4,18 @@ namespace backend.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class HelloWorldController : Controller
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    [HttpGet]
+    public string Get() => "Hello world test!";
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
+    [HttpGet("{id:int:min(10)}")] // /HelloWorld/10/HelloWorld/10
+    public IActionResult GetById(int id) {
+        return  Ok(new { message = "Hello world! " + id.ToString()});
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+    [HttpGet("param")]  //  /HelloWorld/param?id=50&rg=12345
+    public IActionResult GetFromParam([FromQuery] int id, int rg=5) {
+        return Ok(new { message = string.Format("Id is: {0} and Rg is: {1}", id, rg)});
     }
 }
